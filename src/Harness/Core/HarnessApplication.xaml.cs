edcd33101c7d7cabb704432076111c53440f836e
Rod P <rodp@live.com.au>
2021-07-08 00:23:21 +1000

    Update solution projects - Harness skeleton

---
+++ src/Harness/Core/HarnessApplication.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Rox
+{
+    public partial class HarnessApplication : Application
+    {
+        public HarnessApplication()
+        {
+            InitializeComponent();
+
+            MainPage = new MainView();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
