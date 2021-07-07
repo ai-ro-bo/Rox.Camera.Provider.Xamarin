bbcb8d2bab6b62df49ffe9351da8c1059af142f5
Rod P <rodp@live.com.au>
2021-07-08 00:36:01 +1000

    Update solution projects - Add Shared folder and assembly files.

--- src/Harness/Android/Properties/AssemblyProject.cs
+++ src/Harness/Android/Properties/AssemblyProject.cs
@@ -1,24 +1,3 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: System.Reflection.AssemblyTitle("Rox Camera Control Harness for Android")]
+﻿[assembly: System.Reflection.AssemblyTitle("Rox Camera Control Harness for Android")]
 [assembly: Android.App.UsesPermission(Android.Manifest.Permission.Internet)]
 [assembly: Android.App.UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
-
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Core.Android")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/Shared/AssemblyVersion.cs
@@ -0,0 +1,3 @@
+﻿[assembly: System.Reflection.AssemblyVersion("2.0.0.0")]
+[assembly: System.Reflection.AssemblyFileVersion("2.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersion("2.0.0.0")]
