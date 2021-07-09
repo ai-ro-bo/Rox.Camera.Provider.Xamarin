dcaf6e02327eee6de465177bd758abe75a7806df
Rod <Rod@RetailDirections>
2021-07-09 16:18:20 +1000

    Version Update

--- src/Shared/AssemblyVersion.cs
+++ src/Shared/AssemblyVersion.cs
@@ -1,3 +1,3 @@
-﻿[assembly: System.Reflection.AssemblyVersion("2.0.0.0")]
-[assembly: System.Reflection.AssemblyFileVersion("2.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersion("2.0.0.0")]
+﻿[assembly: System.Reflection.AssemblyVersion("5.0.0.0")]
+[assembly: System.Reflection.AssemblyFileVersion("5.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersion("5.0.0.0")]
